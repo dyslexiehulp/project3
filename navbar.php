6179b395880dae30c3598838d711d5108263820c
TOAD163 <toad@toadcraft.net>
2019-03-13 10:18:07 +0100

    updated everything

---
+++ navbar.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="./index.php">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-item nav-link" href="./index.php">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-item nav-link" href="./info.php">Informatie</a>
+        <a class="nav-item nav-link" href="./oefenen.php">Oefeningen</a>
+        <a class="nav-item nav-link" href="./inloggen.php">Inloggen</a>
+        <a class="nav-item nav-link" href="./overons.php">Over ons</a>
+      </div>
+    </div>
+  </nav>
