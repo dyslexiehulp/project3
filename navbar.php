1911f76e10caecf7a20ed31c989397fd4a9327f9
TOAD163 <toad@toadcraft.net>
2019-03-20 11:18:34 +0100

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,11 +1,11 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="./index.php"><img src="./image/Logo.png, 200, 200"></a>
+    <a class="navbar-brand" href="./index.php"><img src="./image/Logo.png",200,200></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <a class="nav-item nav-link" href="./index.php">Home <span class="sr-only">(current)</span></a>
+        <!--<a class="nav-item nav-link" href="./index.php">Home <span class="sr-only">(current)</span></a>-->
         <a class="nav-item nav-link" href="./info.php">Informatie</a>
         <a class="nav-item nav-link" href="./oefenen.php">Oefeningen</a>
         <a class="nav-item nav-link" href="./inloggen.php">Inloggen</a>
